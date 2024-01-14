cf7027df40f7f9f2a95de417842916e345e837d0
jessie <jessie.pastan@gmail.com>
2024-01-13 23:06:14 -0500

    delete unuse struct and @state items

--- FoodFridge/Core/Pantry/PantryView.swift
+++ FoodFridge/Core/Pantry/PantryView.swift
@@ -13,60 +13,47 @@ struct PantryView: View {
   
     @FocusState private var isTextFieldFocused: Bool
     @State private var isEditing = false
-    @State private var items: [ListItem] = [
-        ListItem(id: UUID(), text: "Item 1"),
-        ListItem(id: UUID(), text: "Item 2"),
-        ListItem(id: UUID(), text: "Item 3")
-    ]
+ 
     @State private var text = ""
     @State private var editingItemId: Int?
     
     var body: some View {
         
         Text("Your pantry")
-        Text("edit id : \(editingItemId ?? 100)")
+        Text("(for testing) edit id : \(editingItemId ?? 100)")
         
         
-        List($vm.pantryItems.indices, id: \.self) { index in
-            
-            
-            
-            if PantryItem.mockPantryItem[index].id == editingItemId    {
-                HStack {
-                    TextField((PantryItem.mockPantryItem[index].itemName), text: $text, onCommit: { self.editingItemId = nil
-                    })
-                    
-                    
-                    Button {
-                        self.editingItemId = nil
-                        //MARK:TODO:
-                        //and save new data to database
-                        
-                    } label: {
-                        Text("Done")
-                        
-                    }
-                    
-                }
-            } else {
-                HStack {
-                    Text(PantryItem.mockPantryItem[index].itemName)
-                    Spacer()
-                    Button {
-                        isEditing = true
-                        self.editingItemId = PantryItem.mockPantryItem[index].id
-                    } label: {
-                        Text("Edit")
-                      
-                    }
-                }
-                
-            }
-            
-            
-        }
-        //.onDelete(perform: deleteItems)
-        //.onMove(perform: moveItems)
+        List {
+            Section(header: Text(Date(), style: .date)) {
+                     ForEach($vm.pantryItems.indices, id: \.self) { index in
+                         if PantryItem.mockPantryItem[index].id == editingItemId {
+                             HStack {
+                                 TextField(PantryItem.mockPantryItem[index].itemName, text: $text, onCommit: {
+                                     self.editingItemId = nil
+                                     // Handle the commit action here
+                                 })
+                                 Button {
+                                     self.editingItemId = nil
+                                     //MARK: TODO: Save new data to database
+                                 } label: {
+                                     Text("Done")
+                                 }
+                             }
+                         } else {
+                             HStack {
+                                 Text(PantryItem.mockPantryItem[index].itemName)
+                                 Spacer()
+                                 Button {
+                                     isEditing = true
+                                     self.editingItemId = PantryItem.mockPantryItem[index].id
+                                 } label: {
+                                     Text("Edit")
+                                 }
+                             }
+                         }
+                     }.onDelete(perform: deleteItems)
+                 }
+             }
         
     }
     
@@ -78,20 +65,8 @@ struct PantryView: View {
         vm.pantryItems.remove(atOffsets: offsets)
     }
     
-    func moveItems(from source: IndexSet, to destination: Int) {
-        vm.pantryItems.move(fromOffsets: source, toOffset: destination)
-    }
-    
-    func editItem() {
-        
-    }
     
     
-}
-
-struct ListItem: Identifiable {
-    let id: UUID
-    var text: String
 }
 
 #Preview {
