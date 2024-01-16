30fe42ebb7528a02fb79efd277a74ca617c7265b
jessie <jessie.pastan@gmail.com>
2024-01-16 00:06:29 -0500

    add Ingredient struct as model

--- FoodFridge/Model/IngredientItem.swift
+++ FoodFridge/Model/IngredientItem.swift
@@ -2,23 +2,7 @@
 //  IngredientItem.swift
 //  FoodFridge
 //
-//  Created by Jessie Pastan on 1/15/24.
+//  Created by Jessie Pastan on 1/16/24.
 //
 
 import Foundation
-
-
-struct IngredientItem: Codable, Identifiable, Hashable {
-    let id: String
-    var name: String
-    var type: String
-}
-
-enum CodingKeys: String, CodingKey {
-           case id = "ingredient_id"
-           case name = "ingredient_name"
-           case type = "ingredient_type_code"
-}
-    
-
-
