452566722938ee766c1b6c06128abfcd2761ce5b
jessie <jessie.pastan@gmail.com>
2024-01-25 17:57:16 -0500

    implement link for each recipe to recipe view

---
+++ FoodFridge/Core/ResultPage/RecipesView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipesView.swift
+//  FoodFridge
+//
+//  Created by Jessie Pastan on 1/25/24.
+//
+
+import SwiftUI
+
+struct RecipesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipesView()
+}
