71649745dbf9c8a843d6b5887995ba40c0b9c4c1
jessie <jessie.pastan@gmail.com>
2024-01-25 16:55:59 -0500

    implement recipe row

---
+++ FoodFridge/Core/Component/RecipeRow.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeRow.swift
+//  FoodFridge
+//
+//  Created by Jessie Pastan on 1/25/24.
+//
+
+import SwiftUI
+
+struct RecipeRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeRow()
+}
