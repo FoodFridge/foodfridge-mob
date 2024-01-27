f0c2d13866fd8edfb2ec7840a11780cd649b477c
jessie <jessie.pastan@gmail.com>
2024-01-26 22:17:36 -0500

    implement google recipes view

--- FoodFridge/Core/Component/GoogleResultRow.swift
+++ FoodFridge/Core/Component/GoogleResultRow.swift
@@ -12,49 +12,89 @@ struct GoogleResultRow: View {
     var title: String
     var link: String
     var img: String
+    @Binding var isLiked: Bool
+    @Environment(\.openURL) var openURL
     
     var body: some View {
         ZStack {
             Rectangle()
             HStack {
-                
-                Image(systemName: "heart")
-                    .foregroundStyle(.black)
-                    .padding(10)
-                
-                Text(title)
-                    .foregroundStyle(.black)
-                    .font(.custom(CustomFont.appFontRegular.rawValue, size: 17))
-                    .padding()
-                Spacer()
-                AsyncImage(url: URL(string: img)) { Image in
-                    Image
-                        .resizable()
-                        .scaledToFit()
-                        .backgroundStyle(.white)
-                        .cornerRadius(10)
-                        .shadow(radius: 5, x: 5, y:5)
-                        
-                } placeholder: {
-                    
+                Button {
+                    // tap to save URL
+                    isLiked.toggle()
+                } label: {
+                    Image(systemName:  isLiked ?  "heart.fill" : "heart" )
+                        .foregroundStyle(.black)
+                        .padding(10)
                 }
-                .padding(.horizontal)
+                
+                Button {
+                    // tap to navigate to google link
+                    if let googleLink = URL(string: link) {
+                        openURL(googleLink)
+                    }
+                } label: {
+                    VStack {
+                        Text(title)
+                            .foregroundStyle(.black)
+                            .font(.custom(CustomFont.appFontRegular.rawValue, size: 17))
+                            .padding()
+                            .multilineTextAlignment(.leading)
+                    }
+                }
                
-                    
+                Spacer()
                 
-                
+                Button {
+                    // tap to navigate to google link
+                    if let googleLink = URL(string: link) {
+                        openURL(googleLink)
+                    }
+                } label: {
+                    AsyncImage(url: URL(string: img)) { phase in
+                        switch phase {
+                        case .empty:
+                            // Placeholder when the image is not yet loaded
+                            ProgressView()
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .cornerRadius(10)
+                                .frame(maxWidth: 70, maxHeight: 90)
+                                .scaledToFill()
+                                .backgroundStyle(.white)
+                                .shadow(radius: 5, x: 5, y: 5)
+                        case .failure(_):
+                            // Placeholder or error handling when the image fails to load
+                            Image(systemName: "photo")
+                                .resizable()
+                                .cornerRadius(10)
+                                .frame(maxWidth: 70, maxHeight: 90)
+                                .scaledToFill()
+                                .backgroundStyle(.white)
+                                .shadow(radius: 5, x: 5, y: 5)
+                        @unknown default:
+                            // Placeholder or default handling for unknown cases
+                            ProgressView()
+                        }
+                    }
+                    .aspectRatio(contentMode: .fill)
+                    .padding(.horizontal)
+                }
+               
             }
-               
-            
+    
         }
         .frame(width: 350, height: 100)
         .shadow(radius: 8, x: 5, y:5)
         .cornerRadius(10)
         .foregroundStyle(.button2)
         
+        
+      
     }
 }
 
 #Preview {
-    GoogleResultRow(title: "Soy Ginger Salmon {Fast, Healthy Asian Salmon Recipe ...", link: "https://www.wellplated.com/soy-ginger-salmon/", img: "https://www.wellplated.com/wp-content/uploads/2017/04/Baked-Soy-Ginger-Salmon.jpg")
+    GoogleResultRow(title: "Soy Ginger Salmon {Fast, Healthy Asian Salmon Recipe ...", link: "https://www.wellplated.com/soy-ginger-salmon/", img: "https://www.wellplated.com/wp-content/uploads/2017/04/Baked-Soy-Ginger-Salmon.jpg", isLiked: .constant(false))
 }
--- FoodFridge/Core/ResultPage/RecipesView.swift
+++ FoodFridge/Core/ResultPage/RecipesView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct RecipesView: View {
     var title: String = "Salmon with Ginger Glaze"
+    var googleRecipes = GoogleSearchRecipe.mockGoogleSearchRecipes
+    @State private var isLiked = false
     var body: some View {
+        VStack {
+            ScrollView {
+                ForEach(googleRecipes) { recipe in
+                   
+                    GoogleResultRow(title: recipe.title, link: recipe.link, img: recipe.img, isLiked: $isLiked)
+                }
+            }.scrollIndicators(.hidden)
+            
+        }
+        
         
         Text("List of \(title) Recipes by google search")
         
