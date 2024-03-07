ecdba5487db8276ffe29bca37ec1f738aa230f3e
jessie <jessie.pastan@gmail.com>
2024-03-06 19:01:06 -0500

    add addPantryView

--- FoodFridge/Core/Pantry/AddPantryView.swift
+++ FoodFridge/Core/Pantry/AddPantryView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct AddPantryView: View {
+    
+    @StateObject private var vm = AddPantryViewModel()
+    @EnvironmentObject var sessionManager: SessionManager
+    
     var body: some View {
-        Text("Add pantry")
+        VStack(spacing: 1.2) {
+            Spacer()
+                ZStack{
+                    HStack(spacing: 0) {
+                        TextField("Enter item", text: $vm.searchText)
+                            .padding()
+                            .autocapitalization(.none)
+                            //.background(Color.button4)
+                            .foregroundColor(.white)
+                            .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.button1, lineWidth: 5))
+                        Spacer()
+                        Button {
+                            //MARK: TODO add pantry
+                            if !vm.searchText.isEmpty {
+                                vm.addPantry(sessionManager: sessionManager, item: vm.searchText)
+                            }
+                        } label: {
+                            Text("Add")
+                                .padding(.horizontal)
+                                .frame(height: 62)
+                                .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.button2, lineWidth: 5))
+                            
+                        }
+                    }
+                }
+                
+                   // Display suggestions
+                List  {
+                        ForEach(vm.suggestions, id: \.self) { suggestion in
+                            Text(suggestion)
+                                .onTapGesture {
+                                    vm.searchText = suggestion // Update the text field with the selected suggestion
+                                }
+                        }
+                        .listRowBackground(Color.button3)
+                        .foregroundColor(.black)
+                        
+                        
+                    }
+                    .listStyle(PlainListStyle()) // Remove default List padding/styling
+                  
+            Spacer()
+               }
+               .scrollContentBackground(.hidden)
+               .frame(maxWidth: .infinity)
+               .padding()
+       
     }
 }
 
---
+++ FoodFridge/Core/Pantry/AddPantryViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  AddPantryViewModel.swift
+//  FoodFridge
+//
+//  Created by Jessie Pastan on 3/6/24.
+//
+
+import Foundation
+class AddPantryViewModel: ObservableObject {
+        @Published var searchText = ""
+        @Published var suggestions: [String] = []
+        
+        // Sample list of ingredients
+        let allIngredients = ["Apple","Apple pie", "Apple smith","Apple fuji","Apple green","Apple vinigar","Apricot","Banana","Thai banana", "Carrot", "Dates", "Eggplant", "Fig", "Grapes"]
+        
+        init() {
+            // Update suggestions whenever searchText changes
+            $searchText
+                .map { text in
+                    if text.isEmpty {
+                        return []
+                    } else {
+                        return self.allIngredients.filter { $0.lowercased().contains(text.lowercased()) }
+                    }
+                }
+                .assign(to: &$suggestions)
+        }
+    
+    func addPantry(sessionManager: SessionManager, item: String) {
+    
+            Task {
+                try await AddPantry(sessionManager: sessionManager).addPantry(with: item)
+            }
+            
+    }
+    
+}
