c7cf22a9b35d52cfa4e5c12921531e16936ee688
jessie <jessie.pastan@gmail.com>
2024-03-26 21:32:52 -0400

    fix padding

--- FoodFridge/Core/Authentication/AuthenticationView.swift
+++ FoodFridge/Core/Authentication/AuthenticationView.swift
@@ -26,128 +26,130 @@ struct AuthenticationView: View {
     
    
     var body: some View {
-        NavigationStack  {
-            if sessionManager.getAuthToken() != nil {
-                LandingPageView()
-            } else {
-                GeometryReader { proxy in
-                    VStack {
-                        HStack {
+       
+            NavigationStack  {
+                if sessionManager.getAuthToken() != nil {
+                    LandingPageView()
+                } else {
+                    GeometryReader { proxy in
+                        VStack {
+                            HStack {
+                                
+                                Spacer()
+                                //MARK: Image
+                                Image("cooking")
+                                    .resizable()
+                                    .frame(width: 270, height: 270)
+                                    .scaledToFit()
+                                    .offset(x: 20, y: 20)
+                                Spacer()
+                                
+                                //MARK: Skip link to landing page
+                                
+                                NavigationLink {
+                                    
+                                    LandingPageView()
+                                    
+                                } label: {
+                                    
+                                    Text("Skip")
+                                    
+                                }
+                                .padding(.trailing, -10)
+                                .offset(y: -130)
+                                .foregroundStyle(Color(.button4))
+                                
+                            }
+                            VStack {
+                                Text("Ready to explore new menu ?")
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    .lineLimit(2)
+                                    .multilineTextAlignment(.center)
+                                    .padding(.leading, 35)
+                                
+                            }
+                            .font(Font.custom("CourierPrime-Bold", size: 26))
+                            .padding(.top, 10)
+                            .frame(width: proxy.size.width)
+                            .padding(.leading, -30)
+                            
+                            //MARK: Buttons for create account and sign in
+                            //create account with email
+                            VStack {
+                                LoginButton(title:"Create account") {
+                                    self.signUpWithEmailShow = true
+                                }
+                                .sheet(isPresented: $signUpWithEmailShow, content: {
+                                    SignUpWithEmailView()
+                                })
+                                
+                                
+                                LoginButton(title:"Sign in with Email") {
+                                    self.logInPageShow = true
+                                }
+                                .sheet(isPresented: $logInPageShow, content: {
+                                    LogInView()
+                                })
+                            }
+                            .padding(.leading,-30)
+                            .font(Font.custom("CourierPrime-Regular", size: 25))
+                            
+                            Text("or")
+                                .padding([.top,.bottom], 1)
+                                .font(Font.custom("CourierPrime-Regular", size: 17))
+                            
+                            //continue with gg and apple
+                            VStack {
+                                GoogleSignInButton {
+                                    GoogleSignInHelper(sessionManager: sessionManager).SignInWithGoogle(from: getRootViewController())
+                                }
+                                
+                                
+                                switch colorScheme {
+                                case .dark:
+                                    SignInWithAppleButton { request in
+                                        appleSignIn.handleSignInWithAppleRequest(request)
+                                    } onCompletion: { result  in
+                                        appleSignIn.handleSignInWithAppleCompletion(result)
+                                    }
+                                    .signInWithAppleButtonStyle(.white)
+                                    .frame(width: 330, height: 50)
+                                    .cornerRadius(120)
+                                case .light:
+                                    SignInWithAppleButton { request in
+                                        appleSignIn.handleSignInWithAppleRequest(request)
+                                    } onCompletion: { result  in
+                                        appleSignIn.handleSignInWithAppleCompletion(result)
+                                    }
+                                    .signInWithAppleButtonStyle(.black)
+                                    .frame(width: 330, height: 50)
+                                    .cornerRadius(120)
+                                @unknown default:
+                                    SignInWithAppleButton { request in
+                                        appleSignIn.handleSignInWithAppleRequest(request)
+                                    } onCompletion: { result  in
+                                        appleSignIn.handleSignInWithAppleCompletion(result)
+                                    }
+                                    .signInWithAppleButtonStyle(.whiteOutline)
+                                    .frame(width: 330, height: 50)
+                                    .cornerRadius(120)
+                                }
+                                
+                                
+                            }
                             
                             Spacer()
-                            //MARK: Image
-                            Image("cooking")
-                                .resizable()
-                                .frame(width: 270, height: 270)
-                                .scaledToFit()
-                                .offset(x: 20, y: 20)
-                            Spacer()
-                            
-                            //MARK: Skip link to landing page
-                            
-                             NavigationLink {
-                             
-                                 LandingPageView()
-                                 
-                             } label: {
-                                 
-                             Text("Skip")
-                                 
-                             }
-                             .padding(.trailing, -10)
-                             .offset(y: -130)
-                             .foregroundStyle(Color(.button4))
                             
                         }
-                        VStack {
-                            Text("Ready to explore new menu ?")
-                                .fixedSize(horizontal: false, vertical: true)
-                                .lineLimit(2)
-                                .multilineTextAlignment(.center)
-                                .padding(.leading, 35)
-                            
-                        }
-                        .font(Font.custom("CourierPrime-Bold", size: 26))
-                        .padding(.top, 10)
-                        .frame(width: proxy.size.width)
-                        .padding(.leading, -30)
-                        
-                        //MARK: Buttons for create account and sign in
-                        //create account with email
-                        VStack {
-                            LoginButton(title:"Create account") {
-                                self.signUpWithEmailShow = true
-                            }
-                            .sheet(isPresented: $signUpWithEmailShow, content: {
-                                SignUpWithEmailView()
-                            })
-                            
-                            
-                            LoginButton(title:"Sign in with Email") {
-                                self.logInPageShow = true
-                            }
-                            .sheet(isPresented: $logInPageShow, content: {
-                                LogInView()
-                            })
-                        }
-                        .padding(.leading,-30)
-                        .font(Font.custom("CourierPrime-Regular", size: 25))
-                        
-                        Text("or")
-                            .padding([.top,.bottom], 10)
-                            .font(Font.custom("CourierPrime-Regular", size: 17))
-                        
-                        
-                        VStack {
-                            GoogleSignInButton {
-                                GoogleSignInHelper(sessionManager: sessionManager).SignInWithGoogle(from: getRootViewController())
-                            }
-                            
-                            
-                            switch colorScheme {
-                            case .dark:
-                                SignInWithAppleButton { request in
-                                    appleSignIn.handleSignInWithAppleRequest(request)
-                                } onCompletion: { result  in
-                                    appleSignIn.handleSignInWithAppleCompletion(result)
-                                }
-                                .signInWithAppleButtonStyle(.white)
-                                .frame(width: 330, height: 50)
-                                .cornerRadius(120)
-                            case .light:
-                                SignInWithAppleButton { request in
-                                    appleSignIn.handleSignInWithAppleRequest(request)
-                                } onCompletion: { result  in
-                                    appleSignIn.handleSignInWithAppleCompletion(result)
-                                }
-                                .signInWithAppleButtonStyle(.black)
-                                .frame(width: 330, height: 50)
-                                .cornerRadius(120)
-                            @unknown default:
-                                SignInWithAppleButton { request in
-                                    appleSignIn.handleSignInWithAppleRequest(request)
-                                } onCompletion: { result  in
-                                    appleSignIn.handleSignInWithAppleCompletion(result)
-                                }
-                                .signInWithAppleButtonStyle(.whiteOutline)
-                                .frame(width: 330, height: 50)
-                                .cornerRadius(120)
-                            }
-                          
-
-                        }
-                        
-                        Spacer()
-                        
+                        .padding()
                     }
-                    .padding()
                 }
             }
+            .navigationBarBackButtonHidden(true)
+           
         }
-        .navigationBarBackButtonHidden(true)  
-    }
+        
     
-   
 }
 
 
