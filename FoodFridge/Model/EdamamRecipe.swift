6a63bbfd3222a570340590fd62089479cc044598
jessie <jessie.pastan@gmail.com>
2024-05-03 16:24:08 -0400

    add mock recipes

--- FoodFridge/Model/EdamamRecipe.swift
+++ FoodFridge/Model/EdamamRecipe.swift
@@ -16,13 +16,20 @@ struct EdamamRecipe: Codable, Identifiable {
     let userId: String?
     
     enum CodingKeys: String, CodingKey {
-               case id
-               case title
-               case img
-               case isFavorite = "favorite_status"
-               case link
-               case userId = "local_Id"
+        case id
+        case title
+        case img
+        case isFavorite = "favorite_status"
+        case link
+        case userId = "local_Id"
     }
+}
     
-    
+extension EdamamRecipe {
+    static var mockRecipes: [EdamamRecipe] {
+            [EdamamRecipe(id: "1", title: "Pork omelet", img: "https://www.youtube.com/watch?v=J2hficUEUcE",link: "", isFavorite: "https://i.ytimg.com/vi/C5WBACYnSd8/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBT5ZX6qXOgwmWy5o0Xvm_gJqOyqA", userId: "test user"),
+             EdamamRecipe(id: "2", title: "Crispy Omelet", img: "https://www.youtube.com/watch?v=-6zXcZjSCE4", link: "", isFavorite: "https://i.ytimg.com/vi/C5WBACYnSd8/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBT5ZX6qXOgwmWy5o0Xvm_gJqOyqA", userId: "test user")
+            ]
+        }
+        
 }
